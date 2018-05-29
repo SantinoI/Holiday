50381f912d103c0f82eba4105827ca943597f9f9
Santino Isgrò <santiisgro96@gmail.com>
2018-05-29 13:33:08 +0200

    Aggiunto rilevamento posizione del device

--- lap2-prj/screens/Home.js
+++ lap2-prj/screens/Home.js
@@ -1,37 +1,99 @@
-import React from 'react';
-import { StyleSheet,
-        Text,
-        ScrollView,
-        View, FlatList,
-        TouchableHighlight,
-        Button,
-        TextInput } from 'react-native';
-import {MaterialIcons} from "@expo/vector-icons";
-import {StackNavigator} from "react-navigation";
-import {TabNavigator} from "react-navigation";
+import React from "react";
+import {
+  StyleSheet,
+  Platform,
+  Text,
+  ScrollView,
+  View,
+  FlatList,
+  TouchableHighlight,
+  TextInput
+} from "react-native";
+import { Permissions, Location } from "expo";
+
+import { MaterialIcons } from "@expo/vector-icons";
+import { StackNavigator } from "react-navigation";
+import { TabNavigator } from "react-navigation";
+
+import { SearchBar, Button } from "react-native-elements";
 
 import * as firebase from "firebase";
 
 const TINT_COLOR = "rgb(4, 159, 239)";
 
 export default class Home extends React.Component {
-    render() {
-      return (
-        <ScrollView>
-         <Button
-            title="Trova Escursioni"
+  state = {
+    text: "",
+    address: "",
+    location:""
+  };
 
-         />
-        </ScrollView>
-      );
-    }
+  async componentWillMount(){
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status !== "granted") {
+        alert("You need to enable the GPS and authorize it");
+        return;
+      }else{
+        let location = await Location.getCurrentPositionAsync();
+        console.log(location);
+        this.setState({ location: location.coords, isMapVisible: true });
+        let address = await Location.reverseGeocodeAsync(location.coords);
+        this.setState({
+          address: address[0].city + ", " + address[0].name
+        });
+        console.log(address);
+      }
   }
 
-  Home.navigationOptions = ({ navigation }) => {
-    return {
-      title: "Home",
-      headerStyle: {
-        
-      },
-    };
+  // Funzione che passa come parametro il contenuto della searchBar alla navigation quando viene premuto il button search
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.searchContainer}>
+          <SearchBar
+            containerStyle={styles.searchBar}
+            onChangeText={value => this.setState({ text: value })}
+            lightTheme
+            showLoading
+            searchIcon={true}
+            placeholder="Type Here..."
+            Platform={Platform.OS === "ios" ? "ios" : "android"}
+            cancelIcon={{ type: "font-awesome", name: "chevron-left" }}
+          />
+          <Text>{this.state.text}</Text>
+          <Button
+            style={{ marginTop: 50 }}
+            // loading = {this.state.isLoading}
+            raised
+            backgroundColor={TINT_COLOR}
+            title="Trova Escursioni"
+            //onPress={this.onRequest} // Ricerca
+          />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+Home.navigationOptions = ({ navigation }) => {
+  return {
+    title: "Home",
+    headerStyle: {}
   };
+};
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    flex: 1,
+    marginTop: 150,
+    alignItems: "stretch",
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1"
+  },
+  searchBar: {
+    marginLeft: 10,
+    marginRight: 10,
+    backgroundColor: "white"
+  }
+});
