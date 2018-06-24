f650fe19a4ec0bb999f6f15bba7ddc75f0e04aee
Gioele <gcageggi@gmail.com>
2018-06-24 09:39:30 +0200

    Grafica Login Page

--- lap2-prj/screens/Login.js
+++ lap2-prj/screens/Login.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
-import { View, Text } from "react-native";
-import { Card, Button, FormLabel, FormInput } from "react-native-elements";
+import {
+  StyleSheet,
+  Platform,
+  
+  ScrollView,
+  View,
+  FlatList,
+  TouchableHighlight,
+  TouchableOpacity,
+  TextInput,
+  Dimensions
+} from "react-native";
+import { Button, FormLabel, FormInput } from "react-native-elements";
+import { Text, Form, Item, Label, Input, Content, Card, CardItem, Thumbnail, Left, Body, Right, Container } from 'native-base';
+import { StackNavigator } from "react-navigation";
+import { TabNavigator } from "react-navigation";
 
 import * as firebase from "firebase";
 
 
 const TINT_COLOR = "rgb(4, 159, 239)";
+const BACKGROUND_COLOR = "#d7e4e5";
+const TINT_COLOR2 = "#39b9c3";
 
-export default class LoginForm extends React.Component {
+
+export default class Login extends React.Component {
   static navigationOptions = {
     title: "Login"
   };
@@ -51,9 +68,11 @@ export default class LoginForm extends React.Component {
       });
   }
 
+  
+
   renderLoginOrSpinner() {
     return (
-      <View style={{ justifyContent: "space-between", height: "40%"}}>
+      <View style={{ backgroundColor: BACKGROUND_COLOR, justifyContent: "space-between", height: "40%"}}>
         <Button style={{marginTop: 50}}
           loading = {this.state.isLoading}
           raised
@@ -62,7 +81,7 @@ export default class LoginForm extends React.Component {
           onPress={this._login}
         />
 
-        <Button style={{marginTop: 50}}
+        <Button style={{marginTop: 40}}
           loading = {this.state.isLoading}
           raised
           backgroundColor = {TINT_COLOR}
@@ -76,27 +95,104 @@ export default class LoginForm extends React.Component {
 
   render() {
     return (
-      <View>
-        <Card style={{flex: 1}}>
-          <FormLabel>E-mail</FormLabel>
+      
+      <Container style={{padding:25, backgroundColor : BACKGROUND_COLOR}}>
+        
+          <Card style={{padding:30, borderRadius: 10}}>
+                
+                  <Form>
+                    <Item floatingLabel>
+                      <Label style={{}} >E-mail</Label>
+                      <Input  onChangeText={text => this.setState({ email: text })}
+                               //value={this.state.email} 
+                      />
+                    </Item>
+                    <Item floatingLabel last style={{marginTop:25}} >
+                      <Label>Password</Label>
+                      <Input  onChangeText={text => this.setState({ password: text })}
+                               //value={this.state.password} 
+                      />
+                    </Item>
+                  </Form>
+                
+                  
+                  <View style={styles.buttonContainer}>
+                      <TouchableOpacity
+                          style={styles.searchButton}
+                          activeOpacity={0.5}
+                          title="Login"
+                      >
+                        <Text style={{textAlign:'center', color: "white" }}> Login </Text>
+                      </TouchableOpacity>
+                  </View>
+
+                  <View style={styles.buttonContainer}>
+                      <TouchableOpacity
+                          style={styles.searchButton}
+                          activeOpacity={0.5}
+                          title="Registrati"
+                      >
+                        <Text style={{textAlign:'center', color: "white" }}> Registrati </Text>
+                      </TouchableOpacity>
+                  </View>
+                
+
+
+
+             </Card>
+             
+    </Container>
+    );
+  }
+  
+}
+
+Login.navigationOptions = ({ navigation }) => {
+  return {
+    title: "Login",
+    headerStyle: {
+      backgroundColor: BACKGROUND_COLOR,
+      borderBottomWidth: 0
+    },
+    
+  };
+};
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginTop: 35,
+    justifyContent: "center"
+  },
+
+  searchButton: {
+    
+    padding: 10,
+    backgroundColor: TINT_COLOR2,
+    borderRadius: 30,
+  },
+});
+
+{/* <View >
+        
+            <FormLabel>E-mail</FormLabel>
             <FormInput
               label="E-mail"
-              placeholder="enter a valid e-mail"
+              placeholder="Inserisci la tua email"
               onChangeText={text => this.setState({ email: text })}
               //value={this.state.email}
             />
-          <FormLabel>Password</FormLabel>
+            <FormLabel>Password</FormLabel>
             <FormInput
               label="password"
-              placeholder="enter password"
+              placeholder="Inserisci password"
               onChangeText={text => this.setState({ password: text })}
               //value={this.state.password}
             />
           
           {this.renderLoginOrSpinner()}
           <Text>{this.state.error}</Text>
-        </Card>
-      </View>
-    );
-  }
-}
+          
+        
+      </View> */}
+
+
